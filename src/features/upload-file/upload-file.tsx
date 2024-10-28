bb56fcbcd93f04def50dd6cf0c826350c9a2fc72
NieWang <superexplorernw@163.com>
2024-10-28 14:58:38 +0800

    feat(009): enhancement - device-control

--- src/features/upload-file/upload-file.tsx
+++ src/features/upload-file/upload-file.tsx
@@ -1,27 +1,9 @@
-import { useState } from "react";
 export const loader = () => {
     return {};
 }
 
 export default function UploadFile() {
-    const [list, updateList] = useState<{ id: number; name: string }[]>([]);
-
     return (
-        <>
-            <div>
-                <ul className="flex gap-2">
-                    {
-                        list.map((item, index) => {
-                            return (
-                                <li key={index} className="flex items-center justify-center">
-                                    <span>{item.id}-{item.name}</span>
-                                    <span className="icon-[material-symbols--close] cursor-pointer relative top-[2px]" style={{ color: "#bcbcbc" }} onClick={() => { updateList(() => list.filter(({ id }) => id !== item.id)) }} ></span>
-                                </li>
-                            );
-                        })
-                    }
-                </ul>
-            </div>
-        </>
+        <></>
     );
 }
