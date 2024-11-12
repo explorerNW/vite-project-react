41d4599cdec716d3e8b076afddec785a827d7ab2
explorerNW <32231114+explorerNW@users.noreply.github.com>
2024-11-12 11:52:11 +0800

    feat(013): enhancement - add vite - babel config and algorithm - binarySearch (#16)
    * feat(013): enhancement - add vite - babel config and algorithm - binarySearch
    
    * feat(013): enhancement - vite - babel config and algorithm - binarySearch

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['antd'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/antd/, /node_modules/],
+    },
+  },
 });
