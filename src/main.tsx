f20972097d1da98218d028559b0bc9755bfe06b4
explorerNW <32231114+explorerNW@users.noreply.github.com>
2024-11-25 10:43:28 +0800

    perf(023): performance - add webpack to build (#27)
    * perf(023): performance - add webpack to build
    
    * perf(023): performance - add webpack to build

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
-import router from './router.tsx';
+import router from './router';
 import { Provider } from 'react-redux';
-import store from './store.tsx';
+import store from './store';
 import { ConfigProvider } from 'antd';
 import './instrument';
+import './styles/tailwind.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
